394fcbc3b9eb3a895c97c1b582eb214cc530a560
Vedant <paratevedant1403@gmail.com>
2026-05-27 12:45:46 +0530

    update CORS for production

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      "http://localhost:5173",
+      "https://study-notion-git-master-vedantparates-projects.vercel.app",
+    ],
     credentials: true,
   })
 );
